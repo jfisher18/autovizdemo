ac03621eb4cceba5380fc5e2a8f3506d9fc23788
jfisher18 <windowwipermonster@gmail.com>
2020-02-12 20:49:38 -0500

    Saving rework work

--- server.js
+++ server.js
@@ -1,94 +1,70 @@
 const sqlite3 = require('sqlite3').verbose();
-
-Date.prototype.addDays = function(days) {
-    var date = new Date(this.valueOf());
-    date.setDate(date.getDate() + days);
-    return date;
-}
-
-function twoDigit(num){
-  if (num>=10){
-    return num.toString();
-  } else {
-    return "0"+num.toString();
-  }
-}
-
-var queryMap = new Map();
-//669
-var sliderRange = [0,669];
-var step = 1;
-let graphRanges = [[0,100],[101,300],[301,669]];//will need to actually compute
-
-//precompute map of values
-let db = new sqlite3.Database('./crashes.db', (err) => {
-  if (err) {
-    console.error(err.message);
-  }
-  console.log('DB Connected');
-  var startDate = new Date('January 2, 2018');
-  var dateLimit = new Date('November 2, 2019');
-  for (let i = sliderRange[0]; i <=sliderRange[1]; i++){
-    dateLimit = startDate.addDays(i)
-    dateStr = `${dateLimit.getFullYear()}-${twoDigit(dateLimit.getMonth()+1)}-${twoDigit(dateLimit.getDate())}`
-    queryStr = 'SELECT borough, COUNT(*) as crashes FROM CRASHES WHERE borough!="" AND date<=? GROUP BY borough;'
-    db.all(queryStr, [dateStr], (err, rows) => {
-      queryMap.set(i, {data: rows, query: queryStr})
-      console.log(i)
-    })
-  }
-});
-
-let scalesComputed = false;
-
-function computeScales(){
-  for (let range of graphRanges){
-    for (let i = range[0]; i <= range[1]; i++){
-      var max = 0;
-      var data_obj = queryMap.get(i);
-      for (let borough of data_obj.data){
-        if (borough.crashes > max){
-          max = borough.crashes
-        }
-      }
-      let scale_domain = [0,max];
-      for (let i = range[0]; i <= range[1]; i++){
-        var data_obj = queryMap.get(i);
-        data_obj.scale_domain = scale_domain
-        queryMap.set(i,data_obj)
-      }
-    }
-
-  }
-  scalesComputed = true;
-}
-
-
-
-
 const express = require('express');
 const app = express();
-const port = 3000
+const port = 3000;
 var http = require('http').createServer(app);
+var formidable = require('formidable');
+var fs = require('fs');
 var io = require('socket.io')(http);
+var ejs = require('ejs');
+app.set('view_engine', 'ejs');
 
 var path = require('path');
 
 app.use(express.static('public'))
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname , 'views', '/index.html'));
 })
 
+app.get('/newdb', (req, res) => {
+    res.sendFile(path.join(__dirname , 'views', '/newdb.html'));
+})
 
-io.on('connection', function(socket){
-  socket.emit('init',sliderRange);
-  socket.on('query', function(index){
-    if(!scalesComputed){
-      computeScales();
-    }
-    socket.emit('data', queryMap.get(parseInt(index)))
+app.post('/uploaddb', (req, res) => {
+  var form = new formidable.IncomingForm();
+  form.parse(req, (err, fields, files) => {
+    fs.mkdir(path.join('dbs',fields.dbname), { recursive: true },  (err) => {
+      if (err) throw err;
+    });
+    var fileloc = files.db.path;
+    var newpath = path.join(__dirname, 'dbs', fields.dbname, (fields.dbname + ".db"));
+    fs.rename(fileloc, newpath, (err) => {
+    if (err) throw err;
+      let db = new sqlite3.Database(newpath, (err) => {
+        db.get(fields.query, [parseInt(fields.start)], (err, row) => {
+          if (err) throw err;
+          // Render ejs with keys from object
+        })
+      })
+    });
+    var textInfo = fields.query.trim();
+    textInfo += "\n" + fields.start;
+    textInfo += "\n" + fields.stop;
+    textInfo += "\n" + fields.step;
+    textInfo += "\n" + fields.breakpoints.trim();
+    var textPath = path.join(__dirname, 'dbs', fields.dbname, (fields.dbname + ".txt"));
+    fs.writeFile(textPath, textInfo, function (err) {
+      if (err) throw err;
+    });
   });
+})
+
+app.post('/submitfields', (req, res) => {
+  //For each possible value, save results to .csv file (for fields we want)
+  //Go through and calculate the scales, using breakpoints
+  //Add to db list
 });
+
+// io.on('connection', function(socket){
+//   socket.emit('init',sliderRange);
+//   socket.on('query', function(index){
+//     if(!scalesComputed){
+//       computeScales();
+//     }
+//     socket.emit('data', queryMap.get(parseInt(index)))
+//   });
+// });
 
 http.listen(port, () => console.log(`Listening on port ${port}!`))
